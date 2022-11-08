4880a0fcd57dccc85658642b6a424076599af11b
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-11-08 21:17:50 +0000

    initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "./views/404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("text").send("404 Not Found");
+  }
+});
+
+app.listen(PORT, () => console.log(`✔ Server running on port ${PORT}`));
